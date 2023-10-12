d331187c4481a38abad939893f78d551370a9a6f
noahwhlim <noahhlimwh@gmail.com>
2023-10-12 17:06:32 -0500

    ebay-clone

---
+++ app/hooks/useCreateAddress.js
@@ -0,0 +1,28 @@
+const useCreateAddress = async (details) => {
+  console.log(details)
+  let url = 'create'
+
+  // if there is an addressId then change create to update
+  if (details.addressId)  {
+    url = 'update'
+  }
+
+  const response = await fetch(`/api/address/${url}`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      addressId: details.addressId,
+      name: details.name,
+      address: details.address,
+      zipcode: details.zipcode,
+      city: details.city,
+      country: details.country,
+    })
+  })
+
+  const data = await response.json()
+
+  return data 
+}
+
+export default useCreateAddress
